3b7f860eb5ad906bc654fb549e028cda204357e9
yuenkuan <yuenkuan@192.168.1.102>
2020-07-23 10:52:11 +0800

    P4Q4

---
+++ src/Owner.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yuenkuan
+ */
+public class Owner {
+    
+    private String name;
+    private String icNo;
+    
+    public Owner(String name,String icNo){
+        this.name = name;
+        this.icNo = icNo;
+    }
+    
+    public String toString(){
+       return String.format("%-15s %-15s", this.name, this.icNo);
+    }
+    
+}
